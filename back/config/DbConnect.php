22a03a926b19f6d41de5b5797ea3768a211e5df5
Maria Luisa Moreno <maria.moreno@unifesp.br>
2021-05-14 10:58:17 -0300

    Adição do banco de dados

---
+++ back/config/DbConnect.php
@@ -0,0 +1,14 @@
+<?php 
+class DbConnect{
+    function connect($environment){ 
+        $dbhost="localhost";
+        $dbuser="sem-user";
+        $dbpass="sem-user";
+        $dbname="case";
+        $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $conn;
+    }
+}
+?>
---
+++ database/02adding_some_data.sql
@@ -0,0 +1,4 @@
+-- Data
+INSERT INTO `status_promo` (`STATUS`) VALUES ('AVAIABLE');
+INSERT INTO `status_promo` (`STATUS`) VALUES ('PAUSED');
+INSERT INTO `status_promo` (`STATUS`) VALUES ('NOT AVAIABLE');
