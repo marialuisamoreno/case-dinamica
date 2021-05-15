cadd06441fbda8bc405db6743c07e22981832a31
Maria Luisa Moreno <maria.moreno@unifesp.br>
2021-05-15 08:39:49 -0300

    Adição da página de add product

--- front/src/components/routes.js
+++ front/src/components/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Products from './pages/products/products';
 import AddProducts from './pages/products/add';
-import ProductsDetails from './pages/products/products-details';
 import Catalog from './pages/client/catalog';
 
 const Routes = () => (
@@ -11,7 +10,6 @@ const Routes = () => (
             <Route exact path="/" component={Products} />
             <Route exact path="/products/main/products" component={Products} />
             <Route exact path="/products/main/products/add" component={AddProducts} />
-            <Route exact path="/products/main/products-details/:id" component={ProductsDetails} />
             <Route exact path="/client/catalog" component={Catalog} />
         </Switch>
     </BrowserRouter>
